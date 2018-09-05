033cd0450b1e9c3d8a4a7d272e5d621ea7869071
Ivan Malkov <ozonep@yandex.ru>
2018-09-05 11:23:23 +0300

    Replaced bcrypt with another package

--- PostAnswer/index.js
+++ PostAnswer/index.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 const decomment = require('decomment');
 const cuid = require('cuid');
 const pgp = require('pg-promise')();
+const stringHash = require('string-hash');
 
 const cn = {
     host: process.env.PostgresDBHost, // default: localhost
@@ -36,28 +37,26 @@ module.exports = function (context, req) {
         }
         const noCommentsAnswer = decomment(req.body.answer);
         const normalizedAnswer = noCommentsAnswer.replace(/\s+/g, ' ').trim();
-        bcrypt.hash(normalizedAnswer, 10).then((hash) => {
-            db.tx(t => t.batch([
-                t.none('INSERT INTO student_answers (answer_id, user_id, answer) VALUES ($1, $2, $3)', [uniqueId, req.body.userId, req.body.answer]),
-                t.none('INSERT INTO answers (id, normalized_answer, hash, exercise_id) VALUES ($1, $2, $3, $4)', [uniqueId, normalizedAnswer, hash, req.body.exerciseId]),
-            ]))
-                .then(() => {
-                    context.res = {
-                        status: 200,
-                        body: uniqueId
-                    };
-                    context.done();
-                })
-                .catch(err => {
-                    context.res = {
-                        status: 400,
-                        body: err.message
-                    };
-                    context.done();
-                });
-        });
-    } 
-    else {
+        const hashedAnswer = stringHash(normalizedAnswer);
+        db.tx(t => t.batch([
+            t.none('INSERT INTO student_answers (answer_id, user_id, answer) VALUES ($1, $2, $3)', [uniqueId, req.body.userId, req.body.answer]),
+            t.none('INSERT INTO answers (id, normalized_answer, hash, exercise_id) VALUES ($1, $2, $3, $4)', [uniqueId, normalizedAnswer, hashedAnswer, req.body.exerciseId]),
+        ]))
+            .then(() => {
+                context.res = {
+                    status: 200,
+                    body: uniqueId
+                };
+                context.done();
+            })
+            .catch(err => {
+                context.res = {
+                    status: 400,
+                    body: err.message
+                };
+                context.done();
+            });
+    } else {
         context.res = {
             status: 400,
             body: "No body im request"
